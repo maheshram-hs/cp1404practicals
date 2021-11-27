c0d8b5f9905cf4eb94fbf1f82bf4039619893c69
Maheshram <smaheshram@yahoo.com>
2021-11-27 21:58:25 +0530

    Add ASCII table with choice of columns.

--- prac_02/ascii_table.py
+++ prac_02/ascii_table.py
@@ -24,9 +24,29 @@ def main():
     print(f"The character for {ascii_code} is {chr(ascii_code)}")
 
     # ASCII table.
-    print("\nCode   Character\n")
-    for i in range(LOWER, UPPER + 1):
-        print(f"{i:3} {chr(i):>8}")
+    # print("\nCode   Character\n")
+    # for i in range(LOWER, UPPER + 1):
+    #   print(f"{i:3} {chr(i):>8}")
+
+    # ASCII table with columns.
+    print("\n<<< ASCII Table >>>\n")
+    columns = int(input("Number of columns: "))
+    print()
+
+    total_ascii = UPPER - LOWER + 1
+    rows = total_ascii // columns
+
+    ascii_number = LOWER
+    for row in range(rows):
+        for column in range(columns):
+            print(f"{ascii_number:8} {chr(ascii_number):>2}", end="")
+            ascii_number += 1
+        print()
+
+    continue_ascii = ascii_number
+    for i in range(continue_ascii, UPPER + 1):
+        print(f"{i:8} {chr(i):>2}", end="")
+    print("\n")
 
 
 if __name__ == "__main__":
