f1ffc23995d082b564ecc750473073ece8426547
Maheshram <smaheshram@yahoo.com>
2021-12-03 12:42:45 +0530

    Write the scores to file.

--- prac_03/scores.py
+++ prac_03/scores.py
@@ -8,11 +8,13 @@ import random
 
 def main():
     """Get number of scores and generate that many random scores."""
+    out_file = open("results.txt", 'w')
     num_of_scores = int(input("Enter number of scores: "))
     for i in range(1, num_of_scores + 1):
         score = random.randint(1, 100)
         result = score_result(score)
-        print(f"{i}) {score} is {result}")
+        print(f"{score} is {result}", file=out_file)
+    out_file.close()
 
 
 def score_result(score):
