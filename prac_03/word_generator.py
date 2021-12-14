402a3d3bf753781722068ce55b79aa1545f63eb6
Maheshram <smaheshram@yahoo.com>
2021-12-14 16:07:15 +0530

    Add function to validate the user's input.

--- prac_03/word_generator.py
+++ prac_03/word_generator.py
@@ -6,25 +6,35 @@ Another way to get just consonants would be to use string.ascii_lowercase
 (all letters) and remove the vowels.
 """
 import random
-import string
 
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
-alphabet = string.ascii_letters
 
-# word_format = input("Enter word format: ").lower()  # "ccvcvvc"
 
-word_format = ""
-for i in range(random.randint(1, 12)):
-    word_format += random.choice(["#", "%", random.choice(alphabet).lower()])  # Now that's truly random ;)
+def main():
+    """..."""
+    word_format = input("Enter word format: ").lower()  # Example input: "ccvcvvc"
+    while not is_valid_format(word_format):
+        print("Invalid word format, Example input: ccvcvvc")
+        word_format = input("Enter word format: ").lower()
 
-word = ""
-for kind in word_format:
-    if kind == "#":
-        word += random.choice(CONSONANTS)
-    elif kind == "%":
-        word += random.choice(VOWELS)
-    else:
-        word += kind
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        elif kind == "v":  # Use else-statement when error checking function is added.
+            word += random.choice(VOWELS)
+    print(word)
 
-print(word)
+
+def is_valid_format(sequence):
+    for char in sequence:
+        if char == "c" or char == "v":
+            pass
+        else:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    main()
