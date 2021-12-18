1e7fc52d993c76918714eb9e557ab3e992d9128f
Maheshram <smaheshram@yahoo.com>
2021-12-18 10:39:39 +0530

    Reformat code.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,15 +6,24 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
+
+def main():
+    """Get state initials and print the name."""
+    print(CODE_TO_NAME)
+
     state_code = input("Enter short state: ").upper()
+    while state_code != "":
+        if state_code in CODE_TO_NAME:
+            print(state_code, "is", CODE_TO_NAME[state_code])
+        else:
+            print("Invalid short state")
+        state_code = input("Enter short state: ").upper()
 
-for key, value in CODE_TO_NAME.items():
-    print(f"{key:3} is {value}")
+    # Print all state's initials and names.
+    for key, value in CODE_TO_NAME.items():
+        print(f"{key:3} is {value}")
+
+
+if __name__ == '__main__':
+    main()
