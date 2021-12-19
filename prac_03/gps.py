f78b180552d805c7465bf6363d17547089054711
Maheshram <smaheshram@yahoo.com>
2021-12-19 12:58:46 +0530

    Add Gopher Population Simulator.

---
+++ prac_03/gps.py
@@ -0,0 +1,16 @@
+"""
+Program that simulates a population of gophers over a ten-year period.
+
+GPS (Gopher Population Simulator). Created by Maheshram Shunmuganand, December 2021
+"""
+
+
+def main():
+    """..."""
+    gophers = 1000
+    for i in range(10):
+        pass
+
+
+if __name__ == '__main__':
+    main()
