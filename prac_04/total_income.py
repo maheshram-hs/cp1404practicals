058ffa7e7034e1871320a0dd5dccfa0494fe036f
Maheshram <smaheshram@yahoo.com>
2021-12-20 18:33:11 +0530

    Refactor print_report function to use enumerate.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,16 +13,17 @@ def main():
         income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes, number_of_months):
+def print_report(incomes):
+    """Print thr cumulative total income."""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month + 1, income, total))
 
 
-main()
+if __name__ == '__main__':
+    main()
