03b8ac891cbb9f8c8f7af0703b8992c59c53dfd3
Maheshram <smaheshram@yahoo.com>
2021-12-21 18:34:12 +0530

    Botch code to handle exceptions.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,20 +9,27 @@ def main():
     """Basic list operations."""
     numbers = []
     index = 1
-    number = int(input(f"Number {index}: "))
-    while number >= 0:
-        index += 1
+    try:
         number = int(input(f"Number {index}: "))
-        if number < 0:
-            break
-        numbers.append(number)
+        if number >= 0:
+            while number >= 0:
+                index += 1
+                number = int(input(f"Number {index}: "))
+                if number < 0:
+                    break
+                numbers.append(number)
 
-    # Print output.
-    print(f"The first number: {numbers[0]}")
-    print(f"The last number: {numbers[-1]}")
-    print(f"The smallest number: {min(numbers)}")
-    print(f"The largest number: {max(numbers)}")
-    print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+            # Print output.
+            print(f"The first number: {numbers[0]}")
+            print(f"The last number: {numbers[-1]}")
+            print(f"The smallest number: {min(numbers)}")
+            print(f"The largest number: {max(numbers)}")
+            print(f"The average of the numbers is {sum(numbers) / len(numbers):.0f}")
+
+        print("Thank you.")
+
+    except ValueError:
+        print("Invalid input.")
 
     # Check username.
     usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
