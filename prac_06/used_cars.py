2c4f0531c582b219a5808d8adc95380ceebd992c
Maheshram <smaheshram@yahoo.com>
2021-12-28 18:35:53 +0530

    Illustrate __str__ method used in Car class.

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,14 +6,27 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
+    my_car = Car("Car", 180)
+    my_car.drive(30)  # fuel: 150
+
+    print("\n* Car *\n")
+
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
-    print(my_car)
+    print(f"\n{my_car}")
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    print("\n* Limo *\n")
 
-main()
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(f"fuel: {limo.fuel}")
+    limo.drive(115)
+    print(f"odo: {limo.odometer}")  # fuel: 5
+    print(limo)
+
+
+if __name__ == '__main__':
+    main()
