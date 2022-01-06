65e8c5a68bcac77eebf66adfee0f1cce9bc93798
Maheshram Shunmuganand <smaheshram@yahoo.com>
2022-01-07 00:46:53 +0530

    Add guitars.py

---
+++ prac_06/guitars.py
@@ -0,0 +1,15 @@
+"""
+Program to use the Guitar class.
+
+Guitars. Created by Maheshram Shunmuganand, January 2022
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Play the guitars (not really;)"""
+
+
+if __name__ == '__main__':
+    main()
