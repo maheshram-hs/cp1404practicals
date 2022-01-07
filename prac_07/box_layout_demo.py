8f97af5e6b85db261bd310d7106c3f3a7a63d302
Maheshram Shunmuganand <smaheshram@yahoo.com>
2022-01-08 02:05:27 +0530

    Add greet and clear function.

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -8,5 +8,13 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("greet")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_text.text
+
+    def handle_clear(self):
+        self.root.ids.input_text.text = ""
+        self.root.ids.output_label.text = "Enter your name"
+
 
 BoxLayoutDemo().run()
