3b1a0fd4fb706ac5dacad3593c5483a332f7858e
Maheshram <smaheshram@yahoo.com>
2022-02-10 00:32:27 +0530

    Rename files using shutil.move()

--- prac_09/os_demos.py
+++ prac_09/os_demos.py
@@ -34,11 +34,12 @@ def main():
         print("Renaming {} to {}".format(filename, new_name))
 
         # Try these options one at a time
+
         # Option 1: rename file to new name - in place
-        os.rename(filename, new_name)
+        # os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
+        shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
